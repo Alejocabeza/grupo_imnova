4787b20cf08e695130e584ffd5a6f74354128246
Alejandro Cabeza <alejandroworkoficial@gmail.com>
2023-04-19 07:50:49 -0400

    added modal with bootstrap

---
+++ src/js/Modal.js
@@ -0,0 +1,37 @@
+export class Modal {
+	constructor(article, modal, person) {
+		this.article = document.querySelectorAll(article)
+		this.modal = document.querySelector(modal)
+		this.persons = person
+	}
+
+	handleOpenModal() {
+		this.article.forEach((item) => {
+			item.addEventListener('click', (e) => {
+				if (e.target.matches('.team__button')) {
+					const title = item.querySelector('.team__h3').innerText
+					const person = this.persons.filter((person) => person.name === title)
+					if (person.length > 0) {
+						this.modal.querySelector('.modalCustom__img').setAttribute('src', person[0]['avatar'])
+						this.modal.querySelector('.modalCustom__h3').innerText = person[0]['name']
+						this.modal.querySelector('.modalCustom__h4').innerText = person[0]['puesto']
+						this.modal.querySelector('.modalCustom__p').innerText = person[0]['description']
+						this.modal.classList.toggle('modalCustom--open')
+						this.modal.querySelector('.modalCustom__div').classList.add('modalCustom__div--open')
+					}
+				}
+			})
+		})
+	}
+
+	handleCloseModal(btnClose) {
+		document.addEventListener('click', (e) => {
+			console.log(e.target);
+			if (e.target.matches(btnClose)) {
+				this.modal.querySelector('.modalCustom__div').classList.remove('modalCustom__div--open')
+				this.modal.classList.remove('modalCustom--open');
+			}
+		})
+	}
+
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import { FormValue } from "./FormValue.js"
 import { Header } from "./Header.js"
+import { Modal } from "./Modal.js"
+import { person } from "./data/persons.js"
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -8,7 +10,14 @@ document.addEventListener('DOMContentLoaded', () => {
 	header.handleMenuMobile('.hamburger--button', '.header')
 	header.handleActiveLink('.header__a')
 	// FUNCTIONALITY HEADER
+
 	// FORM LOGIN AND REGISTER
 	const form = new FormValue()
 	form.handleSubmit('#formLogin', '#formSignin');
+	// FORM LOGIN AND REGISTER
+
+	//Open and Close Modal
+	const modal = new Modal('#teamArticle', '#modalCustom', person)
+	modal.handleOpenModal()
+	modal.handleCloseModal('.modalCustom__button')
 })
